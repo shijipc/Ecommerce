c31a7f9187ad11a03faec3d3a9b55d473ae618eb
shiji <shijipc@gmail.com>
2024-09-08 20:29:48 +0530

    view engine set up

---
+++ routes/userRouter.js
@@ -0,0 +1,10 @@
+const express=require("express");
+const router=express.Router();
+const userController=require("../controllers/user/userController");
+
+router.get("/pageNotFound",userController.pageNotFound);
+router.get("/",userController.loadHomepage);
+
+
+module.exports=router;
+
